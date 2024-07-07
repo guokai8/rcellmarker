db0dc165d4077a3eca379ad13de6106f83d00ede
emontele <110833588+emontele@users.noreply.github.com>
2024-07-07 19:14:14 +0200

    Update data.R

--- R/data.R
+++ R/data.R
@@ -13,7 +13,20 @@
 ##' data(human)
 ##' head(humancells)
 "humancells"
-
+##' @title human_celltaxonomy
+##' @description human cell markers (celltaxonomy; https://ngdc.cncb.ac.cn/celltaxonomy/download) for cell type identification 
+##' @format A list of data frame with five columns individually:
+##' \describe{
+##' \item{SYMBOL}{SYMBOL gene IDs}
+##' \item{cellType}{cell type}
+##' \item{tissueType}{tissue Type}
+##' \item{ENTREZID}{ENTREZID gene IDs}
+##' \item{ENSEMBL}{ENSEMBL gene IDs}
+##' }
+##' @examples
+##' data(human_celltaxonomy)
+##' head(humancelltaxonomycells)
+"humancelltaxonomycells"
 ##' @title mouse
 ##' @description mouse cell markers for cell type identification
 ##' @format A list of data frame with five columns individually:
@@ -29,6 +42,20 @@
 ##' data(mouse)
 ##' head(mousecells)
 "mousecells"
+##' @title mouse_celltaxonomy
+##' @description human cell markers (celltaxonomy; https://ngdc.cncb.ac.cn/celltaxonomy/download) for cell type identification 
+##' @format A list of data frame with five columns individually:
+##' \describe{
+##' \item{SYMBOL}{SYMBOL gene IDs}
+##' \item{cellType}{cell type}
+##' \item{tissueType}{tissue Type}
+##' \item{ENTREZID}{ENTREZID gene IDs}
+##' \item{ENSEMBL}{ENSEMBL gene IDs}
+##' }
+##' @examples
+##' data(mouse_celltaxonomy)
+##' head(mousecelltaxonomycells)
+"mousecelltaxonomycells"
 ##' @title rat
 ##' @description rat cell markers for cell type identification
 ##' @format A list of data frame with five columns individually:
